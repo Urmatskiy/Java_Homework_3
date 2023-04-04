1b14e0427083629266b0253d1daf25da6f9d5e16
Urmatskiy <nickiton14@gmail.com>
2023-04-04 09:19:22 +0300

    Выполнил первое задание

--- Homework.java
+++ Homework.java
@@ -1,6 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Homework {
 
     public static void main(String[] args) {
+        List<Integer> digits = new ArrayList<>();
+        digits.add(5);
+        digits.add(4);
+        digits.add(2);
+        digits.add(7);
+        digits.add(6);
+        System.out.println(digits);
+        removeEvenNumber(digits);
+        System.out.println(digits);
         List<String> strings = new ArrayList<>();
         strings.add("string");
         strings.add("40");
@@ -9,12 +21,17 @@ public class Homework {
         removeIntegers(strings);
         System.out.println(strings); // [string, my_string]
     }
-    
+
     static void removeEvenNumber(List<Integer> numbers) {
         // TODO: 31.03.2023 Удалить все четные элементы из списка
+        for (int i = 0; i < numbers.size(); i += 2) {
+            numbers.remove(numbers.get(i));
+            i--;
+        }
+
     }
-    
+
     static void removeIntegers(List<String> strings) {
         // TODO: 31.03.2023 Удалить строки, которые являются целыми числами
     }
-    }
+}
